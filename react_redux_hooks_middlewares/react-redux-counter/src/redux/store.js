e9027cd4418ce6cb9b2857767e722864a6929e22
asionsolver <asionsolver274@gmail.com>
2023-11-24 13:39:47 +0600

    add new react-redux project name like simple todo application and setup this project

--- react_redux_hooks_middlewares/react-redux-counter/src/redux/store.js
+++ react_redux_hooks_middlewares/react-redux-counter/src/redux/store.js
@@ -3,7 +3,8 @@ import rootReducer from "./rootReducer";
 import myLogger from "./middlewares/myLogger";
 
 import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, myLogger)));
 
 export default store;
