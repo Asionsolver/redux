82fb920a0b76ac06ef2e43e1988e3de16806f9e5
asionsolver <asionsolver274@gmail.com>
2023-11-26 11:03:13 +0600

    add new file name like todo reducer.js and Defining Reducers - todo Slice

--- simple_todo_application_in_react_redux/simple-todo-app/src/App.js
+++ simple_todo_application_in_react_redux/simple-todo-app/src/App.js
@@ -3,29 +3,22 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
 
-
 function App() {
   return (
-    <div
-    className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
-    {/* <!-- navbar --> */}
-    <Navbar/>
-    
+    <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
 
-    <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-        {/* <!-- header --> */}
+      <Navbar />
+      <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+
         <Header />
         <hr className="mt-4" />
 
-        {/* <!-- todo list --> */}
-        <TodoList/>
-
+        <TodoList />
         <hr className="mt-4" />
-
-        {/* <!-- footer --> */}
-        <Footer/>
+        
+        <Footer />
+      </div>
     </div>
-</div>
   );
 }
 
---
+++ simple_todo_application_in_react_redux/simple-todo-app/src/redux/todos/action.js
@@ -0,0 +1,54 @@
+import { ADDED, ALLCOMPLETED, CLEARCOMPLETED, COLORSELECTED, DELETE, TOGGLED } from "./actionTypes";
+
+export const added = (todoText) => {
+  return {
+    type: ADDED,
+    payload: todoText,
+  };
+};
+
+export const toggled = (todoId) => {
+  return {
+    type: TOGGLED,
+    payload: todoId,
+  };
+};
+
+
+export const colorSelected = (todoId, color) => {
+  return {
+    type: COLORSELECTED,
+    payload:{
+        todoId,
+        color,
+    }
+  };
+};
+
+
+export const deleted = (todoId) => {
+  return {
+    type: DELETE,
+    payload: todoId,
+  };
+};
+
+
+export const allCompleted = () => {
+    return {
+      type: ALLCOMPLETED,
+    };
+  };
+
+export const clearCompleted = () => {
+    return {
+      type: CLEARCOMPLETED,
+    };
+  };
+
+
+
+
+
+
+
