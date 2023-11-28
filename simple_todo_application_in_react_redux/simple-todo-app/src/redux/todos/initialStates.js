35162a16912b478bc085eeb38f57f33b9f1adc56
asionsolver <asionsolver274@gmail.com>
2023-11-28 09:20:57 +0600

    complete Todo list and Todo UI update

--- simple_todo_application_in_react_redux/simple-todo-app/src/redux/todos/initialStates.js
+++ simple_todo_application_in_react_redux/simple-todo-app/src/redux/todos/initialStates.js
@@ -9,6 +9,16 @@
     text: "Learn Redux",
     completed: false,
     color: "red",
+  },{
+    id: 3,
+    text: "Learn Javascript",
+    completed: true,
+    color: "green",
+  },{
+    id: 4,
+    text: "Learn Javascript",
+    completed: true,
+    color: "yellow",
   }, 
 ];
 
